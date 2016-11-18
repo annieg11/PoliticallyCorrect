7032628b49b6e74e0ab417108229c787059a833c
aanchal <aanchal.g.gupta@gmail.com>
2016-11-18 13:38:28 -0500

    changes to represntative.js

--- migrations/20161116024630-create-representative.js
+++ migrations/20161116024630-create-representative.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       phoneNum: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       url: {
         type: Sequelize.STRING
