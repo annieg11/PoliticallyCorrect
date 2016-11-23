072fb14ac08c26dba4f70f4681cda4e42bba2665
aanchal <aanchal.g.gupta@gmail.com>
2016-11-22 19:07:06 -0500

    district.js datatype changed and made a link to the rep website

--- migrations/20161116035941-create-district.js
+++ migrations/20161116035941-create-district.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       zipCode: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
